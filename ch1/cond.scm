5b6942f47298d633ba0db574732bd72f6b08473c
Emerson Costa <emerson.silva@lais.huol.ufrn.br>
2020-05-17 16:57:15 -0300

    add code from 1.1.8

---
+++ ch1/cond.scm
@@ -0,0 +1,22 @@
+(define (abs x)
+	(cond ((> x 0) x)
+	      ((= x 0) 0)
+	      ((< x 0) (-x))))
+
+(define (abss x)
+  (cond ((< x 0) (- x))
+         (else x)))
+
+(define (absss x)
+  (if (< x 0) (- x) x))
+
+(define (sqrt x)
+	(define (average a b)
+		(/ (+ a b) 2))
+  (define (good-enough? guess)
+    (< (abs (- (square guess) x)) 0.001))
+  (define (improve guess)
+    (average guess (/ x guess)))
+  (define (sqrt-iter guess)
+    (if (good-enough? guess) guess sqrt-iter(improve guess)))
+	(sqrt-iter 1.0))
